d117a581108f1283c6e790eadfd4509d83421789
Sadiq Khan <sadiqkhan3006@gmail.com>
2025-09-13 01:09:08 +0530

    Setting up monorepo

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,7 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+wss.on('connection', (ws) => {
+    ws.on("message", (data) => {
+        console.log("received: ", JSON.stringify(data))
+    })
+})
