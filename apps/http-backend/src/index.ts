9db0505bef8d25bb1990f7a16750e5a9e0e9a2ec
Sadiq Khan <sadiqkhan3006@gmail.com>
2025-09-18 00:32:06 +0530

    setting up backends and prisma db package

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import jwt from "jsonwebtoken"
 const app = express();
+app.post("/signup", () => {
+
+})
+app.post("/signin", () => {
+
+})
+app.post("/room", () => {
+
+})
+
 app.listen("3030", () => {
     console.log("server started");
 })
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,16 @@
 import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, request) => {
+    const url = request.url;
+    if (!url) return;
+    const queryParams = new URLSearchParams(url?.split("?")[1]);
+    const token = queryParams.get("token");
+    if (!token) {
+        ws.send("INvalid token");
+        ws.close();
+    }
     ws.on("message", (data) => {
+
         console.log("received: ", JSON.stringify(data))
     })
 })
