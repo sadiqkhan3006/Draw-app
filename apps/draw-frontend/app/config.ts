a19c7f81e99bd4d55a97e5876a42005660c09041
Sadiq Khan <sadiqkhan3006@gmail.com>
2025-11-07 03:18:22 +0530

    minor error

--- apps/draw-frontend/app/config.ts
+++ apps/draw-frontend/app/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "http://16.170.95.211/api";
-export const WS_URL = "ws://16.170.95.211/ws";
+export const WS_URL = "ws://16.170.95.211/ws/";
 export const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlZjgzMDhiYy00YTNjLTQ1M2EtYTZjMi0xNjNhM2ExOWFiNzQiLCJpYXQiOjE3NTg2NTcwOTV9.st82Tj8I7u3Scybiq8OB-7YQNd4StKh4LQMa5qBrpOk"
